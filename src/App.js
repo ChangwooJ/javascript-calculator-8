de7463c2ef7b241336464e650a0126ec612c0579
changwooj <wjdckddn99@naver.com>
2025-10-16 13:24:50 +0900

    feat(separator): 커스텀 구분자 처리 기능 구현
    - 입력 문자열이 '//'로 시작하면 '\n' 전까지를 커스텀 구분자로 인식
    - 인식된 구분자를 기본 구분자 [',', ':'] 배열에 추가
    - '\n'이 없으면 오류 발생
    - 구분자 처리가 끝난 배열을 반환

--- src/App.js
+++ src/App.js
@@ -2,19 +2,38 @@ import { Console } from "@woowacourse/mission-utils";
 
 function isNotNumberSeparator(separator) {
   if (/\d/.test(separator)) {
-    throw new Error('[ERROR] 숫자는 구분자에 포함될 수 없습니다.');
+    throw new Error("[ERROR] 숫자는 구분자에 포함될 수 없습니다.");
   }
   return separator;
 }
 
+function extractCustomSeparator(userInput) {
+  const defaultSeparators = [",", ":"];
+
+  if (userInput.startsWith("//")) {
+    const lastSeparatorIndex = userInput.indexOf("\n");
+
+    if (lastSeparatorIndex === -1) {
+      throw new Error(
+        `[ERROR] 커스텀 구분자 정의 시 '\n'을 이용한 마침 표현을 사용해야 합니다.`
+      );
+    }
+
+    const customSeparatorArray = userInput.slice(2, lastSeparatorIndex);
+    defaultSeparators.push(...customSeparatorArray);
+
+    return defaultSeparators;
+  }
+}
+
 class App {
   async run() {
-    Console.print('덧셈할 문자열을 입력해 주세요.');
+    Console.print("덧셈할 문자열을 입력해 주세요.");
     const userInput = Console.readLineAsync();
 
     // 빈 문자열 예외 처리
-    if (userInput === '') {
-      Console.print('결과 : 0');
+    if (userInput === "") {
+      Console.print("결과 : 0");
       return;
     }
   }
