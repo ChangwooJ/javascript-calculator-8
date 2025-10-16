673d559d00913664463248552c071973b6ee40be
changwooj <wjdckddn99@naver.com>
2025-10-16 11:09:23 +0900

    feat(separator): 숫자가 구분자에 포함될 수 없도록 예외 처리 기능 추가
    - 숫자를 커스텀 구분자로 입력했을 경우 숫자가 제거되는 오류 발생 가능
    - 숫자가 포함되는 구분자는 구분자로 인정되지 않고 ERROR 반환

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import { Console } from "@woowacourse/mission-utils";
 
+function isNotNumberSeparator(separator) {
+  if (/\d/.test(separator)) {
+    throw new Error('[ERROR] 숫자는 구분자에 포함될 수 없습니다.');
+  }
+  return separator;
+}
+
 class App {
   async run() {
     Console.print('덧셈할 문자열을 입력해 주세요.');
