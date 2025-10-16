2fbdaf146b03bc342a5d7685be8cd453a089feac
changwooj <wjdckddn99@naver.com>
2025-10-16 09:26:26 +0900

    feat(input): 문자열 사용자 입력 기능 추가 및 빈 문자열 예외 처리

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    Console.print('덧셈할 문자열을 입력해 주세요.');
+    const userInput = Console.readLineAsync();
+
+    // 빈 문자열 예외 처리
+    if (userInput === '') {
+      Console.print('결과 : 0');
+      return;
+    }
+  }
 }
 
 export default App;
