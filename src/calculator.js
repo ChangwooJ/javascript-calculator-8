6602349d1a44d9b6ddb11807c6de8df4042149fb
ChangwooJ <wjdckddn99@naver.com>
2025-10-20 19:44:50 +0900

    fix: 각 파일 함수 임포트

--- src/calculator.js
+++ src/calculator.js
@@ -1,8 +1,11 @@
-import { isNumber } from "./validators";
+import { isNumber } from "./validators.js";
 
 export function sumElements(elements) {
   const numbers = elements.map((element) => isNumber(element));
-  const result = numbers.reduce((accumulator, current) => accumulator + current, 0);
+  const result = numbers.reduce(
+    (accumulator, current) => accumulator + current,
+    0
+  );
 
   return `결과 : ${result}`;
 }
--- src/separators.js
+++ src/separators.js
@@ -1,23 +1,23 @@
-import { isNotNumberSeparator } from "./validators";
+import { isNotNumberSeparator } from "./validators.js";
 
 export function extractCustomSeparator(userInput) {
-  const defaultSeparators = [',', ':'];
+  const defaultSeparators = [",", ":"];
   let inputBody = userInput;
 
-  if (userInput.startsWith('//')) {
-    const lastSeparatorIndex = userInput.indexOf('\\n');
+  if (userInput.startsWith("//")) {
+    const lastSeparatorIndex = userInput.indexOf("\\n");
 
     // \n 마침 표현이 누락된 경우
     if (lastSeparatorIndex === -1) {
       throw new Error(
-        '[ERROR] 커스텀 구분자 정의 시 \'\\n\'을 이용한 마침 표현을 사용해야 합니다.'
+        "[ERROR] 커스텀 구분자 정의 시 '\\n'을 이용한 마침 표현을 사용해야 합니다."
       );
     }
 
     const customSeparatorArray = userInput
       .slice(2, lastSeparatorIndex)
-      .split('')
-      .filter((c) => c.trim() !== '');
+      .split("")
+      .filter((c) => c.trim() !== "");
 
     defaultSeparators.push(...customSeparatorArray);
 
@@ -33,8 +33,8 @@ export function splitBySeparators(separators, inputBody) {
   let result = inputBody;
 
   separators.forEach((separator) => {
-    result = result.split(separator).join(' ');
+    result = result.split(separator).join(" ");
   });
 
-  return result.split(' ').map((s) => s.trim());
+  return result.split(" ").map((s) => s.trim());
 }
